f14b2813d461dbb1f6bfa174fb431d4a4083c08f
Michael-Blessing <michael.blessing005@gmail.com>
2025-12-17 23:59:59 +0100

    part two of day 4 and part one of day5

--- day4/Solution.java
+++ day4/Solution.java
@@ -3,35 +3,58 @@ package day4;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
 
 class Solution {
     public static void main(String[] args) {
         String filePath = "day4/tests/input.txt";
         int result = 0;
+        List<IntPair> pairs = new ArrayList<>();
         try {
             List<String> paperGrid = Files.readAllLines(Path.of(filePath));
-            for (int i = 0; i < paperGrid.size(); i++) {
-                for (int j = 0; j < paperGrid.get(i).length(); j++) {
-                    int amountOfPaper = 0;
-                    if (paperGrid.get(i).charAt(j) == '@') {
+            boolean paperRollRemoved = true;
+            while (paperRollRemoved) {
+                paperRollRemoved = false;
+                for (int i = 0; i < paperGrid.size(); i++) {
+                    for (int j = 0; j < paperGrid.get(i).length(); j++) {
+                        int amountOfPaper = 0;
+                        if (paperGrid.get(i).charAt(j) == '@') {
 
-                        for (int k = -1; k <= 1; k++) {
-                            for (int l = -1; l <= 1; l++) {
+                            for (int k = -1; k <= 1; k++) {
+                                for (int l = -1; l <= 1; l++) {
 
-                                int currentY = i + k;
-                                int currentX = j + l;
+                                    int currentY = i + k;
+                                    int currentX = j + l;
 
-                                if (currentY < 0 || currentY >= paperGrid.size() || currentX < 0
-                                        || currentX >= paperGrid.get(i).length() || (currentX == j && currentY == i)) {
-                                    continue;
+                                    if (currentY < 0 || currentY >= paperGrid.size() || currentX < 0
+                                            || currentX >= paperGrid.get(i).length()
+                                            || (currentX == j && currentY == i)) {
+                                        continue;
+                                    }
+
+                                    amountOfPaper += paperGrid.get(currentY).charAt(currentX) == '@' ? 1 : 0;
                                 }
-
-                                amountOfPaper += paperGrid.get(currentY).charAt(currentX) == '@' ? 1 : 0;
+                            }
+                            if (amountOfPaper < 4) {
+                                result++;
+                                pairs.add(new IntPair(i, j));
+                                paperRollRemoved = true;
                             }
                         }
-                        result += amountOfPaper < 4 ? 1 : 0;
                     }
+                }
+
+                char[][] chars = new char[paperGrid.size()][];
+                for (int i = 0; i < paperGrid.size(); i++) {
+                    chars[i] = paperGrid.get(i).toCharArray();
+                }
+                for (IntPair p : pairs) {
+                    chars[p.first()][p.second()] = '.';
+                }
+                paperGrid.clear();
+                for (int i = 0; i < chars.length; i++) {
+                    paperGrid.add(new String(chars[i]));
                 }
             }
         } catch (IOException ex) {
@@ -40,5 +63,8 @@ class Solution {
             System.out.println(String.format("Result: %d", result));
         }
 
+    }
+
+    private record IntPair(int first, int second) {
     }
 }
---
+++ day5/SolutionPartOne.java
@@ -0,0 +1,42 @@
+package day5;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public static void main(String[] args) {
+        String filePath = "day5/tests/input.txt";
+        int result = 0;
+        List<LongPair> ranges = new ArrayList<>();
+        try {
+            List<String> ingredients = Files.readAllLines(Path.of(filePath));
+            for (int i = 0; i < ingredients.size(); i++) {
+                if (ingredients.get(i).contains("-")) {
+                    String[] numbers = ingredients.get(i).split("-");
+                    ranges.add(new LongPair(Long.parseLong(numbers[0]), Long.parseLong(numbers[1])));
+                } else {
+                    if (ingredients.get(i).length() <= 0)
+                        continue;
+                    long currentNum = Long.parseLong(ingredients.get(i));
+                    for (LongPair p : ranges) {
+                        if (p.first() <= currentNum && currentNum <= p.second()) {
+                            result++;
+                            break;
+                        }
+                    }
+                }
+            }
+        } catch (IOException ex) {
+            System.out.println("Some error happened");
+        } finally {
+            System.out.println(String.format("Result: %d", result));
+        }
+
+    }
+
+    private record LongPair(long first, long second) {
+    }
+}
