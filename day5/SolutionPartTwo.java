c77ac2ed822b49cfd3bf905a24c77a24589706e2
Michael-Blessing <michael.blessing005@gmail.com>
2025-12-18 10:26:02 +0100

    day 5 part two

--- day5/SolutionPartTwo.java
+++ day5/SolutionPartTwo.java
@@ -4,31 +4,39 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 class SolutionPartTwo {
     public static void main(String[] args) {
         String filePath = "day5/tests/input.txt";
-        int result = 0;
-        List<LongPair> ranges = new ArrayList<>();
+        long result = 0;
+        ArrayList<Range> ranges = new ArrayList<>();
         try {
             List<String> ingredients = Files.readAllLines(Path.of(filePath));
             for (int i = 0; i < ingredients.size(); i++) {
-                if (ingredients.get(i).contains("-")) {
-                    String[] numbers = ingredients.get(i).split("-");
-                    ranges.add(new LongPair(Long.parseLong(numbers[0]), Long.parseLong(numbers[1])));
+                if (!ingredients.get(i).contains("-"))
+                    continue;
+                String[] numbers = ingredients.get(i).split("-");
+                ranges.add(new Range(Long.parseLong(numbers[0]), Long.parseLong(numbers[1])));
+            }
+
+            ranges.sort(Comparator.comparingLong(r -> r.from));
+
+            long currentStart = ranges.get(0).from;
+            long currentEnd = ranges.get(0).to;
+
+            for (int i = 1; i < ranges.size(); i++) {
+                Range r = ranges.get(i);
+                if (r.from > currentEnd) {
+                    result += (currentEnd - currentStart + 1);
+                    currentStart = r.from;
+                    currentEnd = r.to;
                 } else {
-                    if (ingredients.get(i).length() <= 0)
-                        continue;
-                    long currentNum = Long.parseLong(ingredients.get(i));
-                    for (LongPair p : ranges) {
-                        if (p.first() <= currentNum && currentNum <= p.second()) {
-                            result++;
-                            break;
-                        }
-                    }
+                    currentEnd = Math.max(currentEnd, r.to);
                 }
             }
+            result += (currentEnd - currentStart + 1);
         } catch (IOException ex) {
             System.out.println("Some error happened");
         } finally {
@@ -37,6 +45,7 @@ class SolutionPartTwo {
 
     }
 
-    private record LongPair(long first, long second) {
+    private record Range(long from, long to) {
     }
+
 }
