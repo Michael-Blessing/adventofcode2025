d72860abd224162726ee4cd48b7be74d7c0d5cd9
Michael-Blessing <michael.blessing005@gmail.com>
2025-12-19 21:19:48 +0100

    day 6 part one

---
+++ day6/SolutionPartOne.java
@@ -0,0 +1,36 @@
+package day6;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+class SolutionPartOne {
+    public static void main(String[] args) {
+        String filePath = "day6/tests/input.txt";
+        long result = 0;
+        try {
+            List<String> worksheet = Files.readAllLines(Path.of(filePath));
+            int problemAmount = worksheet.get(0).trim().split("\\s+").length;
+            for (int x = 0; problemAmount > x; x++) {
+
+                String[] operations = worksheet.get(worksheet.size() - 1).trim().split("\\s+");
+                boolean isMultiplication = "*".equals(operations[x]);
+                long currentResult = Integer.parseInt(worksheet.get(0).trim().split("\\s+")[x]);
+                for (int y = 1; y < worksheet.size() - 1; y++) {
+                    if (isMultiplication) {
+                        currentResult *= Integer.parseInt(worksheet.get(y).trim().split("\\s+")[x]);
+                    } else {
+                        currentResult += Integer.parseInt(worksheet.get(y).trim().split("\\s+")[x]);
+                    }
+                }
+                result += currentResult;
+            }
+        } catch (IOException ex) {
+            System.out.println("Some error happened");
+        } finally {
+            System.out.println(String.format("Result: %d", result));
+        }
+
+    }
+}
---
+++ day6/SolutionPartTwo.java
@@ -0,0 +1,51 @@
+package day6;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+class SolutionPartTwo {
+    public static void main(String[] args) {
+        String filePath = "day6/tests/input.txt";
+        long result = 0;
+        ArrayList<Range> ranges = new ArrayList<>();
+        try {
+            List<String> ingredients = Files.readAllLines(Path.of(filePath));
+            for (int i = 0; i < ingredients.size(); i++) {
+                if (!ingredients.get(i).contains("-"))
+                    continue;
+                String[] numbers = ingredients.get(i).split("-");
+                ranges.add(new Range(Long.parseLong(numbers[0]), Long.parseLong(numbers[1])));
+            }
+
+            ranges.sort(Comparator.comparingLong(r -> r.from));
+
+            long currentStart = ranges.get(0).from;
+            long currentEnd = ranges.get(0).to;
+
+            for (int i = 1; i < ranges.size(); i++) {
+                Range r = ranges.get(i);
+                if (r.from > currentEnd) {
+                    result += (currentEnd - currentStart + 1);
+                    currentStart = r.from;
+                    currentEnd = r.to;
+                } else {
+                    currentEnd = Math.max(currentEnd, r.to);
+                }
+            }
+            result += (currentEnd - currentStart + 1);
+        } catch (IOException ex) {
+            System.out.println("Some error happened");
+        } finally {
+            System.out.println(String.format("Result: %d", result));
+        }
+
+    }
+
+    private record Range(long from, long to) {
+    }
+
+}
