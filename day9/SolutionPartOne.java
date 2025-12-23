9a7d95ea85fc1aace79a8ad4861a1dcc2800a13f
user <e12415611@student.tuwien.ac.at>
2025-12-23 20:00:48 +0100

    day 9 part 1

--- day9/SolutionPartOne.java
+++ day9/SolutionPartOne.java
@@ -4,18 +4,16 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 public class SolutionPartOne {
 
     public static void main(String[] args) {
-        String filePath = "day9/tests/input1.txt";
+        String filePath = "day9/tests/input.txt";
         long result = 0;
         try {
             List<String> floor = Files.readAllLines(Path.of(filePath));
-            ArrayList<Tiles> tiles = new ArrayList<>();
+            ArrayList<Tile> tiles = new ArrayList<>();
 
             for (int i = 0; i < floor.size(); i++) {
                 String[] cords = floor.get(i).split(",");
@@ -23,7 +21,10 @@ public class SolutionPartOne {
             }
 
             for (int i = 0; i < tiles.size() - 1; i++) {
-
+                for (int j = i + 1; j < tiles.size(); j++) {
+                    long currentArea = (long) (Math.abs(tiles.get(j).x - tiles.get(i).x) + 1) * (1 + Math.abs(tiles.get(j).y - tiles.get(i).y));
+                    result = Math.max(currentArea, result);
+                }
             }
 
         } catch (IOException ex) {
